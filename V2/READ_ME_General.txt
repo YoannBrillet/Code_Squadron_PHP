28a5cb605fa00e4e71c7457b81d8b0d162045f7d
Yoann Brillet <yoann.brillet@ynov.com>
2017-03-30 10:06:56 +0200

    Start
    Debut du Projet

---
+++ V2/READ_ME_General.txt
@@ -0,0 +1,3 @@
+Il faut exploser les fichers dans le dossier "A MODIFIER" afin de faire une architecture MVC propre.
+Puis definir ce qui des fichiers dans inc vas ou (ou reste la bas).
+Puis on peut tout passer en objet. 
---
+++ V2/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>BUILD IN PROGRESS</title>
+    </head>
+    <body>
+        <h1 align="center">Site en construction</h1>
+        <h3 align="center">veuillez lire les read_me</h4>
+        <h4 align="right">Code Squadron, All rights reserved, 2017</h4>
+    </body>
+</html>
