372d1e570ad99e622db5e90fe61fc5594645421f
Super_tremp <baptiste.fillonneau@ynov.com>
2017-04-13 17:05:41 +0200

    vew mise a jour

--- V2/inc/haut.inc.php
+++ V2/inc/haut.inc.php
@@ -5,6 +5,17 @@
         <title>Mon Site</title>
         <link rel="stylesheet" href="<?php echo RACINE_SITE; ?>inc/css/style.css" />
         <link rel="stylesheet" href="<?php echo RACINE_SITE; ?>inc/css/materialize.css" />
+      	  <meta name="viewport" content="width=device-width, initial-scale=1">
+      	  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+      	  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.3/css/materialize.min.css">
+            <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.3/js/materialize.min.js"></script>
+      	  <script>
+               $(document).ready(function() {
+               $('select').material_select();
+            });
+            </script>
+
     </head>
     <body>
         <header>
@@ -25,15 +36,15 @@
 					if(internauteEstConnecte()) // membre et admin
 					{
 						echo '<a href="' . RACINE_SITE . 'profil.php">Voir votre profil</a>';
-						echo '<a href="' . RACINE_SITE . 'boutique.php">Acc�s � la boutique</a>';
+						echo '<a href="' . RACINE_SITE . 'boutique.php">Accés à la boutique</a>';
 						echo '<a href="' . RACINE_SITE . 'panier.php">Voir votre panier</a>';
-						echo '<a href="' . RACINE_SITE . 'connexion.php?action=deconnexion">Se d�connecter</a>';
+						echo '<a href="' . RACINE_SITE . 'connexion.php?action=deconnexion">Se déconnecter</a>';
 					}
 					else // visiteur
 					{
 						echo '<a href="' . RACINE_SITE . 'inscription.php">Inscription</a>';
 						echo '<a href="' . RACINE_SITE . 'connexion.php">Connexion</a>';
-						echo '<a href="' . RACINE_SITE . 'boutique.php">Acc�s � la boutique</a>';
+						echo '<a href="' . RACINE_SITE . 'boutique.php">Accés à la boutique</a>';
 						echo '<a href="' . RACINE_SITE . 'panier.php">Voir votre panier</a>';
 					}
 					// visiteur=4 liens - membre=4 liens - admin=7 liens
